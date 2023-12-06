bf4c5a538905156fe1032b2b6b38d79a04bad3c4
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-12-07 00:26:53 +0100

    refactor: created primevue plugins config

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,5 +1,4 @@
 import config from "primevue/config";
-import SelectButton from "primevue/selectbutton";
 import Dropdown from "primevue/dropdown";
 import Calendar from "primevue/calendar";
 import Avatar from "primevue/avatar";
@@ -11,22 +10,16 @@ import ConfirmationService from "primevue/confirmationservice";
 import Dialog from "primevue/dialog";
 import ContextMenu from "primevue/contextmenu";
 import AutoComplete from "primevue/autocomplete";
-import Checkbox from "primevue/checkbox";
 import FileUpload from "primevue/fileupload";
 import InputNumber from "primevue/inputnumber";
-import RadioButton from "primevue/radiobutton";
 import InputSwitch from "primevue/inputswitch";
 import MultiSelect from "primevue/multiselect";
 import Tooltip from "primevue/tooltip";
+import Chips from "primevue/chips";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp
-    .use(config, {
-      // ripple: true,
-    })
-    .use(ConfirmationService);
+  nuxtApp.vueApp.use(config).use(ConfirmationService);
 
-  nuxtApp.vueApp.component("PvSelectButton", SelectButton);
   nuxtApp.vueApp.component("PvDropdown", Dropdown);
   nuxtApp.vueApp.component("PvCalendar", Calendar);
   nuxtApp.vueApp.component("PvAvatar", Avatar);
@@ -37,11 +30,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("PvDialog", Dialog);
   nuxtApp.vueApp.component("PvContextMenu", ContextMenu);
   nuxtApp.vueApp.component("PvAutoComplete", AutoComplete);
-  nuxtApp.vueApp.component("PvCheckbox", Checkbox);
   nuxtApp.vueApp.component("PvFileUpload", FileUpload);
   nuxtApp.vueApp.component("PvInputNumber", InputNumber);
-  nuxtApp.vueApp.component("PvRadioButton", RadioButton);
   nuxtApp.vueApp.component("PvInputSwitch", InputSwitch);
   nuxtApp.vueApp.component("PvMultiSelect", MultiSelect);
+  nuxtApp.vueApp.component("PvChips", Chips);
   nuxtApp.vueApp.directive("tooltip", Tooltip);
 });
