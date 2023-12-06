88c8f4f777e0523c3a83fa72cb174bc1087d32c9
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-12-07 00:23:54 +0100

    refactor: added sidebar routes config

---
+++ composables/useSidebarUtils.ts
@@ -0,0 +1,52 @@
+export default () => {
+  const links = ref<
+    {
+      name: string;
+      show?: boolean;
+      icon?: string;
+      route?: string;
+      sub?: { name: string; icon?: string; route?: string; sub?: object[] }[];
+    }[]
+  >([
+    {
+      name: "Dashboard",
+      icon: "view-dashboard",
+      route: "/dashboard"
+    },
+    {
+      name: "Button Samples",
+      icon: "gesture-tap-button",
+      route: "/dashboard/button"
+    },
+    {
+      name: "Input Samples",
+      icon: "form-select",
+      route: "/dashboard/input"
+    },
+    {
+      name: "User",
+      icon: "account",
+      show: false,
+      sub: [
+        {
+          name: "Account",
+          route: "/"
+        },
+        {
+          name: "Settings",
+          route: "/"
+        },
+        {
+          name: "Logout",
+          route: "/"
+        }
+      ]
+    }
+  ]);
+
+  const toggleDropdown = (item: any): void => {
+    item.show = !item.show;
+  };
+
+  return { links, toggleDropdown };
+};
