b83e23fb69c5df48d0d7e4b6398eaec11256f77c
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-12-07 00:20:38 +0100

    refactor: updated tailwind config configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,56 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+ module.exports = {
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#eef1f8',
+        secondary: '#213547',
+        priBg: '#F5E6FE',
+        priText: '#35AF8A',
+        darkBg: '#1E1E2E',
+        darkBgSec: '#27293D'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        urbanist: ['Urbanist', 'sans-serif']
+      },
+      fontSize: {
+        nl: ['56px', '64px']
+      },
+      screens: {
+        '2xs': '200px',
+        '1xs': '350px',
+        '1xl': '1400px',
+        '3xl': '2000px'
+      }
+    }
   },
-  plugins: [],
-}
-
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+    `node_modules/tailvue/dist/tailvue.es.js`,
+    `node_modules/vuewindui/src/components/**/*.vue`,
+  ],
+  // variants: {
+  //   backgroundColor: [
+  //     'dark',
+  //     'dark-hover',
+  //     'dark-group-hover',
+  //     'dark-even',
+  //     'dark-odd'
+  //   ],
+  //   borderColor: ['dark', 'dark-focus', 'dark-focus-within'],
+  //   textColor: ['dark', 'dark-hover', 'dark-active']
+  // }
+};
