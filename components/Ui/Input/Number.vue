1f256ea638e263bed6cea7716c83c63221ab1726
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-12-22 22:07:32 +0100

    feat: implemented number input reusable component

--- components/Ui/Input/Number.vue
+++ components/Ui/Input/Number.vue
@@ -14,6 +14,8 @@ defineProps<{
   currency?: string;
   locale?: string;
   fractionDigits?: number;
+  prependIcon?: string;
+  appendIcon?: string;
 }>();
 </script>
 
@@ -33,6 +35,15 @@ defineProps<{
       <span v-if="required" class="required-mark">*</span>
     </label>
     <div class="relative">
+      <button
+        v-if="prependIcon"
+        type="button"
+        class="absolute inset-y-0 left-0 flex items-center pl-3 icon-button prepend"
+      >
+        <slot name="prependIcon">
+          <Icon :name="prependIcon" class="w-5 h-5 text-gray-400 icon" />
+        </slot>
+      </button>
       <PvInputNumber
         :model-value="value"
         class="!rounded-md w-full"
@@ -48,6 +59,15 @@ defineProps<{
           input: { class: 'input-style ring-0' },
         }"
       ></PvInputNumber>
+      <button
+        v-if="appendIcon"
+        type="button"
+        class="absolute inset-y-0 right-0 flex items-center pr-3 icon-button append"
+      >
+        <slot name="appendIcon">
+          <Icon :name="appendIcon" class="w-5 h-5 text-gray-400 icon" />
+        </slot>
+      </button>
     </div>
     <small v-if="error" class="error-message">{{ error }}</small>
   </Field>
