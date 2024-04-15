ee8ae16316e5265a7a957805c8fad0dcc2e647c7
Taofiq <animashauntaofiq@gmail.com>
2024-04-15 06:14:14 +0100

    refactor: wrapped button in dash wrapper

--- pages/dashboard/button/index.vue
+++ pages/dashboard/button/index.vue
@@ -6,29 +6,31 @@ useHead({
 </script>
 
 <template>
-  <div>
-    <p class="title mb-5 text-4xl">Button Samples</p>
-    <p class="title pb-1.5">Buttons With Icons</p>
-    <div class="flex gap-3.5 items-center flex-wrap">
-      <UiBtn label="Click Me" append-icon="mdi:emoticon" />
-      <UiBtn label="Click Me" prepend-icon="mdi:emoticon" />
-      <UiBtn
-        label="Click Me"
-        append-icon="mdi:emoticon"
-        prepend-icon="mdi:emoticon-excited-outline"
-      />
+  <DashboardWrapper title="Buttons">
+    <div>
+      <p class="title mb-5 text-4xl">Button Samples</p>
+      <p class="title pb-1.5">Buttons With Icons</p>
+      <div class="flex gap-3.5 items-center flex-wrap">
+        <UiBtn label="Click Me" append-icon="mdi:emoticon" />
+        <UiBtn label="Click Me" prepend-icon="mdi:emoticon" />
+        <UiBtn
+          label="Click Me"
+          append-icon="mdi:emoticon"
+          prepend-icon="mdi:emoticon-excited-outline"
+        />
+      </div>
     </div>
-  </div>
-  <div class="mt-5">
-    <p class="title pb-1.5">Buttons With Sizes</p>
-    <div class="flex gap-3.5 items-center flex-wrap">
-      <UiBtn label="Extra Small Button" append-icon="mdi:emoticon" size="xs" />
-      <UiBtn label="Small Button" append-icon="mdi:emoticon" size="sm" />
-      <UiBtn label="Medium Button" append-icon="mdi:emoticon" size="md" />
-      <UiBtn label="Normal Button" append-icon="mdi:emoticon" />
-      <UiBtn label="Large Button" append-icon="mdi:emoticon" size="lg" />
+    <div class="mt-5">
+      <p class="title pb-1.5">Buttons With Sizes</p>
+      <div class="flex gap-3.5 items-center flex-wrap">
+        <UiBtn label="Extra Small Button" append-icon="mdi:emoticon" size="xs" />
+        <UiBtn label="Small Button" append-icon="mdi:emoticon" size="sm" />
+        <UiBtn label="Medium Button" append-icon="mdi:emoticon" size="md" />
+        <UiBtn label="Normal Button" append-icon="mdi:emoticon" />
+        <UiBtn label="Large Button" append-icon="mdi:emoticon" size="lg" />
+      </div>
     </div>
-  </div>
+  </DashboardWrapper>
 </template>
 
 <style scoped></style>
