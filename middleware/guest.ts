07de72026acfc77a55c1c7de3132c5c7f997748d
Taofiq <animashauntaofiq@gmail.com>
2024-07-23 23:28:16 +0100

    feat: added guest middleware

---
+++ middleware/guest.ts
@@ -0,0 +1,16 @@
+export default defineNuxtRouteMiddleware(() => {
+  // Get the current session status from nuxt-auth
+  const { status } = useAuth();
+
+  // Localize the path for the current locale
+  const localPath = useLocalePath();
+
+  // If the user is authenticated, redirect to the index page
+  if (status.value === "authenticated") {
+    return navigateTo(
+      localPath({
+        name: "index"
+      })
+    );
+  }
+});
