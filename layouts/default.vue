2ec06bd5f47e1d73675fde4e4a1ba9b3c2132a60
Taofiq <animashauntaofiq@gmail.com>
2024-07-23 23:26:34 +0100

    update: updated layouts structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
+  <!-- This should be the dashboard if there's auth -->
   <div class="flex flex-col justify-center items-center gap-5 text-center h-screen px-4">
     <slot />
     <NavigationFooter />
