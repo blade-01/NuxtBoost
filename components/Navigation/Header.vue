9e5006913ad7867f252646c4faa5b2d88dedb722
Taofiq <animashauntaofiq@gmail.com>
2024-08-28 15:59:19 +0100

    fix: fixed prod bug

--- components/Navigation/Header.vue
+++ components/Navigation/Header.vue
@@ -4,25 +4,27 @@
   <header class="flex flex-col justify-center items-center text-center">
     <div class="flex justify-between gap-x-8 items-center text-center">
       <div class="flex flex-col items-center gap-2">
-        <nuxt-img
-          provider="cloudinary"
-          src="/nb-light.svg"
-          alt="logo"
-          fit="inside"
-          height="120"
-          width="120"
-          v-if="$colorMode.value === 'dark'"
-        />
-        <nuxt-img
-          provider="cloudinary"
-          src="/nb-dark.svg"
-          alt="logo"
-          fit="inside"
-          height="120"
-          width="120"
-          v-if="$colorMode.value === 'light'"
-        />
-        <p class="text-base font-medium">Nuxt Boost</p>
+        <!-- <div v-if="$colorMode.value">
+          <NuxtImg
+            provider="cloudinary"
+            src="/nb-light.svg"
+            alt="logo"
+            fit="inside"
+            height="120"
+            width="120"
+            v-if="$colorMode?.value === 'dark'"
+          />
+          <NuxtImg
+            provider="cloudinary"
+            src="/nb-dark.svg"
+            alt="logo"
+            fit="inside"
+            height="120"
+            width="120"
+            v-if="$colorMode?.value === 'light'"
+          />
+        </div> -->
+        <h1 class="text-2xl lg:text-4xl font-semibold">Nuxt Boost</h1>
       </div>
     </div>
     <p class="text-lg font-light italic mt-5">{{ $t("title") }}</p>
